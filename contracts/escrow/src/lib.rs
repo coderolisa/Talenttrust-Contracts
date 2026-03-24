ad3e5b61bf1eeb037af6a2202d37ac1102480e53
Isaac <isaacgellu6@gmail.com>
2026-03-24 09:53:08 +0100

    style: fix import formatting for rustfmt
    - Put soroban_sdk imports on single line to match formatting requirements

--- contracts/escrow/src/lib.rs
+++ contracts/escrow/src/lib.rs
@@ -1,9 +1,6 @@
 #![no_std]
 
-use soroban_sdk::{
-    contract, contractimpl, contracttype,
-    Address, Env, Map, Symbol, Vec,
-};
+use soroban_sdk::{contract, contractimpl, contracttype, Address, Env, Map, Symbol, Vec};
 
 use soroban_sdk::symbol_short;
 
